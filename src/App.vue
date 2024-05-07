e2e233f400e06e9e42d8113c6682ab8f4b645c40
VespaRacing <104065786+VespaRacing@users.noreply.github.com>
2024-05-07 09:47:17 +0000

    1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link>
       <router-link to="/Favorite">Favorite</router-link>
+      <router-link to="/">Home</router-link>
       <router-link to="/World">World</router-link>
     </nav>
     <router-view/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,18 @@
 <template>
   <v-container>
-    <h1>Homepage</h1>
+    <h1>Radio By Rampi</h1>
     <v-row>
       <v-col cols="12" sm="6" md="4" lg="3" v-for="radio in radios" :key="radio.id">
-        <v-card class="d-flex flex-row card" flat tile @mouseenter="showControls(radio)"
-          @mouseleave="hideControls(radio)">
+        <v-card class="d-flex flex-row card" flat tile @mouseenter="mostraComandi(radio)"
+          @mouseleave="nascondiComandi(radio)">
           <v-img :src="radio.favicon || defaultImage" class="card-image" :alt="radio.name" />
           <v-card-title class="flex-grow-1">{{ radio.name }}</v-card-title>
           <div v-if="radio.showControls" class="controls">
-            <v-btn @click="togglePlayPause(radio)" :color="radio.playing ? 'error' : 'primary'" small>
-              {{ radio.playing ? 'Pause' : 'Play' }}
+            <v-btn class="bnt-play" @click="cambiaRiproduzione(radio)" :color="radio.playing ? 'error' : 'primary'" small>
+              {{ radio.playing ? '■' : '‣' }}
             </v-btn>
-            <div @click="toggleFavorite(radio)" class="heart-container">
+            <div @click="cambiaPreferito(radio)" class="heart-container">
               <div :class="['heart', { 'liked': radio.favorite }]"></div>
-            </div>
-            <div v-if="radio.playing" class="sound-wave">
-              <div class="bar"></div>
-              <div class="bar"></div>
-              <div class="bar"></div>
-              <div class="bar"></div>
             </div>
           </div>
         </v-card>
@@ -29,10 +23,10 @@
 
 <script>
 import Hls from 'hls.js';
-import defaultImage from '../assets/images (2).jpeg';
+import defaultImage from '../assets/no-image.png';
 
 export default {
-  name: 'HomeView',
+  name: 'PaginaPrincipale',
   data() {
     return {
       radios: [],
@@ -40,7 +34,7 @@ export default {
     }
   },
   methods: {
-    async fetchRadios() {
+    async callAPI() {
       const response = await fetch('https://nl1.api.radio-browser.info/json/stations/search?limit=100&countrycode=IT&hidebroken=true&order=clickcount&reverse=true');
       const data = await response.json();
       return data.filter(radio => radio.countrycode === 'IT').map(radio => ({
@@ -53,94 +47,95 @@ export default {
     },
     async getRadios() {
       try {
-        this.radios = await this.fetchRadios();
-        this.retrieveFavorites();
+        this.radios = await this.callAPI();
+        const preferenze = JSON.parse(localStorage.getItem('preferenze')) || [];
+        this.radios.forEach(radio => {
+        const fav = preferenze.find(f => f.changeuuid === radio.changeuuid);
+        radio.favorite = fav ? fav.favorite : false;
+      });
       } catch (error) {
-        console.error('Error fetching radios:', error);
+        console.error('Errore durante il recupero delle radio:', error);
       }
     },
-    showControls(radio) {
+    mostraComandi(radio) {
       radio.showControls = true;
     },
-    hideControls(radio) {
+    nascondiComandi(radio) {
       radio.showControls = false;
     },
-    togglePlayPause(radio) {
+    cambiaRiproduzione(radio) {
       if (radio.playing) {
-        this.pauseRadio(radio);
+        this.fermaSingola(radio);
       } else {
-        this.pauseAllRadios(); // Pause all other radios
-        this.playRadio(radio);
-      }
-    },
-    playRadio(radio) {
-      const audioUrl = radio.url_resolved || radio.url;
+        this.interrompiTutto(); // Interrompi tutte le altre radio
+        const audioUrl = radio.url_resolved || radio.url;
       if (audioUrl.includes('m3u8')) {
+        //file m3u8
         if (Hls.isSupported()) {
           const hls = new Hls();
           hls.loadSource(audioUrl);
           hls.attachMedia(radio.audioPlayer);
         } else {
-          console.error('HLS is not supported in this browser.');
+          //mp3
+          console.error('HLS non è supportato in questo browser.');
         }
       } else {
         radio.audioPlayer.src = audioUrl;
       }
       radio.audioPlayer.play()
         .catch(error => {
-          console.error('Error playing audio:', error);
+          console.error('Errore durante la riproduzione audio:', error);
           if (error.name === 'NotAllowedError') {
-            console.error('Please ensure that the audio playback is allowed in your browser settings.');
+            console.error('Assicurati che la riproduzione audio sia consentita nelle impostazioni del tuo browser.');
           }
         });
       radio.playing = true;
+      }
     },
-    pauseRadio(radio) {
+    fermaSingola(radio) {
       radio.audioPlayer.pause();
       radio.playing = false;
     },
-    pauseAllRadios() {
+    interrompiTutto() {
       this.radios.forEach(radio => {
         if (radio.playing) {
-          this.pauseRadio(radio);
+          this.fermaSingola(radio);
         }
       });
     },
-    toggleFavorite(radio) {
+    cambiaPreferito(radio) {
       radio.favorite = !radio.favorite;
       if (!radio.favorite) {
-        this.removeFavorite(radio);
+        let preferenze = JSON.parse(localStorage.getItem('preferenze')) || [];
+      preferenze = preferenze.filter(fav => fav.changeuuid !== radio.changeuuid);
+      localStorage.setItem('preferenze', JSON.stringify(preferenze));
       } else {
-        this.saveFavorites();
+        this.salvaPreferito();
       }
     },
-    saveFavorites() {
-      const favorites = this.radios.map(radio => ({ changeuuid: radio.changeuuid, favorite: radio.favorite }));
-      localStorage.setItem('favorites', JSON.stringify(favorites));
-    },
-    removeFavorite(radio) {
-      let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      favorites = favorites.filter(fav => fav.changeuuid !== radio.changeuuid);
-      localStorage.setItem('favorites', JSON.stringify(favorites));
-    },
-    retrieveFavorites() {
-      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      this.radios.forEach(radio => {
-        const fav = favorites.find(f => f.changeuuid === radio.changeuuid);
-        radio.favorite = fav ? fav.favorite : false;
-      });
+    salvaPreferito() {
+      const preferenze = this.radios.map(radio => ({ changeuuid: radio.changeuuid, favorite: radio.favorite }));
+      localStorage.setItem('preferenze', JSON.stringify(preferenze));
+      console.log(JSON.stringify(preferenze));
     },
   },
   created() {
     this.getRadios();
-    this.retrieveFavorites();
+    const preferenze = JSON.parse(localStorage.getItem('preferenze')) || [];
+      this.radios.forEach(radio => {
+        const fav = preferenze.find(f => f.changeuuid === radio.changeuuid);
+        radio.favorite = fav ? fav.favorite : false;
+      });
   },
 }
 </script>
 
+
 <style scoped>
+
 .v-container {
-  background-color: #f5f5f5;
+  background-color: #111111;
+  max-width: 12000px;
   border-radius: 15px;
   padding: 20px;
 }
@@ -155,12 +150,20 @@ export default {
 .heart {
   width: 20px;
   height: 18px;
+  margin-left: -34px;
   background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="%23C1C1C1"/></svg>') center no-repeat;
   background-size: 100%;
 }
 
 .heart.liked {
   background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3z" fill="%23FF0000"/></svg>');
+}
+
+.bnt-play{
+  font-size: 40px;
+  font-al
+  margin-left: -40px;
+  width: 100px;
 }
 
 h1 {
